df851450a7a0cdc81659eac2cd368e8f3861db6b
Hugo Rohach <hugo.rohach@student.ucll.be>
2025-01-17 11:36:48 +0100

    Update

--- flutter_application_1/lib/pages/swipe_card.dart
+++ flutter_application_1/lib/pages/swipe_card.dart
@@ -3,35 +3,47 @@ import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 import 'package:namer_app/model/movie.dart';
 
 class SwipeCard extends StatelessWidget {
-  final Movie movie;
-  SwipeCard({Key? key, required this.movie}) : super(key: key);
+  final List<Movie> movies;
+  final Function(int) onSwipe;
+
+  const SwipeCard({
+    Key? key,
+    required this.movies,
+    required this.onSwipe,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    List<Container> cards = [
-      Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-          image: AssetImage('assets/${movie.title}.jpg'),
-          fit: BoxFit.cover,
-        )),
-      ),
-      Container(
-        alignment: Alignment.center,
-        child: const Text('2'),
-        color: Colors.red,
-      ),
-      Container(
-        alignment: Alignment.center,
-        child: const Text('3'),
-        color: Colors.purple,
-      )
-    ];
     return SizedBox.expand(
       child: CardSwiper(
-        cardsCount: cards.length,
-        cardBuilder: (context, index, percentThresholdX, percentThresholdY) =>
-            SizedBox.expand(child: cards[index]),
+        cardsCount: movies.length,
+        onSwipe: (previousIndex, newIndex, direction) {
+          if (newIndex != null) {
+            onSwipe(newIndex);
+          }
+          return true;
+        },
+        cardBuilder: (context, index, horizontalThreshold, verticalThreshold) {
+          final movie = movies[index];
+          return Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/${movie.title}.jpg'),
+                fit: BoxFit.cover,
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  blurRadius: 8.0,
+                  spreadRadius: 2.0,
+                  offset: Offset(2.0, 4.0),
+                ),
+              ],
+            ),
+            alignment: Alignment.bottomLeft,
+            padding: const EdgeInsets.all(16.0),
+          );
+        },
       ),
     );
   }
--- flutter_application_1/lib/pages/swipe_page.dart
+++ flutter_application_1/lib/pages/swipe_page.dart
@@ -12,31 +12,71 @@ class SwipePage extends StatefulWidget {
 }
 
 class _SwipePageState extends State<SwipePage> {
-  Movie dummyMovie = Movie(
-    title: 'Interstellar',
-    genres: [
-      Genre(name: 'Science Fiction'),
-      Genre(name: 'Horror'),
-      Genre(name: 'Space'),
-    ],
-    producer: 'Christopher Nolan',
-    description:
-        "Hallo Anthonny en Nikolai, alles goed met jullie? Ik heb mijn best gedaan op deze pagina en chatGPT heeft mij goed kunne helpen, danku!",
-  );
+  final List<Movie> movies = [
+    Movie(
+      title: 'Interstellar',
+      genres: [
+        Genre(name: 'Science Fiction'),
+        Genre(name: 'Horror'),
+        Genre(name: 'Space'),
+      ],
+      producer: 'Christopher Nolan',
+      description:
+          "Interstellar is a 2014 epic science fiction drama film directed by Christopher Nolan, who co-wrote the screenplay with his brother Jonathan Nolan.",
+    ),
+    Movie(
+      title: 'Inception',
+      genres: [
+        Genre(name: 'Science Fiction'),
+        Genre(name: 'Thriller'),
+        Genre(name: 'Adventure'),
+      ],
+      producer: 'Christopher Nolan',
+      description:
+          "Inception is a 2010 science fiction action film written and directed by Christopher Nolan, who also produced the film with Emma Thomas.",
+    ),
+    Movie(
+      title: 'The Dark Knight',
+      genres: [
+        Genre(name: 'Action'),
+        Genre(name: 'Drama'),
+        Genre(name: 'Crime'),
+      ],
+      producer: 'Christopher Nolan',
+      description:
+          "The Dark Knight is a 2008 superhero film directed, produced, and co-written by Christopher Nolan.",
+    ),
+  ];
+
+  int currentIndex = 0;
+
+  void handleSwipe(int newIndex) {
+    if (newIndex != null && newIndex >= 0 && newIndex < movies.length) {
+      setState(() {
+        currentIndex = newIndex;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Stack(children: [
-        SizedBox(
-          height: MediaQuery.of(context).size.height,
-          child: SwipeCard(movie: dummyMovie),
+        appBar: AppBar(
+          title: const Text('Swipe'),
         ),
-        GenreInfo(
-          movie: dummyMovie,
-        )
-      ]),
-    ));
+        body: SingleChildScrollView(
+          child: Stack(children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: SwipeCard(
+                movies: movies,
+                onSwipe: handleSwipe,
+              ),
+            ),
+            GenreInfo(
+              movie: movies[1],
+            )
+          ]),
+        ));
   }
 }
